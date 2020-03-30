d5e3ef41e385a7e645cb5e12163b9e2acde3d10b
str1290 <stefanostrr@hotmail.com>
2020-03-30 19:09:24 +0200

    working on uploader

--- src/components/uploader.js
+++ src/components/uploader.js
@@ -2,6 +2,8 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 import { withStyles } from "@material-ui/core/styles";
+import SaveIcon from "@material-ui/icons/Save";
+import BackupIcon from "@material-ui/icons/Backup";
 
 class Uploader extends React.Component {
     constructor(props) {
@@ -20,63 +22,37 @@ class Uploader extends React.Component {
     }
 
     render() {
+        const { classes } = this.props;
         return (
             <div>
-                <div
-                    style={{
-                        width: "100%",
-                        height: "100%",
-                        position: "fixed",
-                        top: 0,
-                        left: 0,
-                        zIndex: 999,
-                        opacity: "0.8",
-                        background: "lightgray"
-                    }}
-                ></div>
+                <div className={classes.container}></div>
                 <div style={{ display: "flex", justifyContent: "center" }}>
-                    <div
-                        style={{
-                            width: "550px",
-                            height: "200px",
-                            backgroundColor: "#8ba9b2",
-                            position: "absolute",
-                            top: "100px",
-                            display: "flex",
-                            justifyContent: "center",
-                            flexDirection: "column",
-                            alignItems: "center",
-                            borderRadius: "20px",
-                            border: "1px solid black",
-                            zIndex: 1000
-                        }}
-                    >
+                    <div className={classes.modal}>
                         <h2>Want to change your image?</h2>
-                        <input
-                            onChange={this.changeImageHandler}
-                            type="file"
-                            style={{
-                                width: "100px",
-                                border: "1px solid #ccc",
-                                display: "inline-block",
-                                padding: "6px 12px"
-                            }}
-                        />
-                        <Button onClick={() => this.props.toggleModal()}>
-                            Close
-                        </Button>
-                        <Button
-                            variant="contained"
-                            color="default"
-                            startIcon={<CloudUploadIcon />}
-                            onClick={() =>
-                                this.props.replaceImage(
-                                    this.state.uploadedImage
-                                )
-                            }
-                        >
-                            UPLOAD
-                        </Button>
+                        <div className={classes.buttonsContainer}>
+                            <BackupIcon />
+                            <input
+                                onChange={this.changeImageHandler}
+                                className="chooseFile"
+                                type="file"
+                            />
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                size="small"
+                                startIcon={<SaveIcon />}
+                                onClick={() =>
+                                    this.props.replaceImage(
+                                        this.state.uploadedImage
+                                    )
+                                }
+                            >
+                                SAVE
+                            </Button>
+                            <Button onClick={() => this.props.toggleModal()}>
+                                Close
+                            </Button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -84,6 +60,38 @@ class Uploader extends React.Component {
     }
 }
 
-const styles = theme => ({});
+const styles = theme => ({
+    container: {
+        width: "100%",
+        height: "100%",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        zIndex: 999,
+        opacity: "0.8",
+        background: "lightgray"
+    },
+    modal: {
+        width: "550px",
+        height: "200px",
+        backgroundColor: "#303f9f",
+        position: "absolute",
+        top: "100px",
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+        borderRadius: "20px",
+        border: "1px solid black",
+        zIndex: 1000
+    },
+    chooseFile: {
+        width: "100px",
+        border: "1px solid #ccc",
+        display: "inline-block",
+        padding: "6px 12px",
+        background: "black"
+    }
+});
 
 export default withStyles(styles)(Uploader);
